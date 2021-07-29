2fdada1780469950d02230e1b09e11f317639c1d
alibek <bajmuhametovalibek30@gmail.com>
2021-07-29 21:09:48 +0300

    anonim user testing

--- spec/views/user/show.html.erb_spec.rb
+++ spec/views/user/show.html.erb_spec.rb
@@ -29,8 +29,26 @@ RSpec.describe 'users/show', type: :view do
   end
 
   context 'not an authorized user' do
+    before(:each) do
+      assign(:user, user)
+      sign_out(user)
+      render
+    end
+
     it 'user does not see change password button' do
       expect(rendered).not_to match 'Сменить имя и пароль'
+    end
+
+    it 'user sees name' do
+      expect(rendered).to match 'Elon'
+    end
+
+    it 'visible fragment game' do
+      assign(:games, game)
+      stub_template 'users/_game.html.erb' => 'User game goes here'
+      render
+
+      expect(rendered).to have_content 'User game goes here'
     end
   end
 end
